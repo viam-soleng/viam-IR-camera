117505e1134b586170c9cf6853575af3aff66eae
Pete <pete@viam.com>
2024-10-15 15:02:18 -0400

    update build script

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 pip3 install -r requirements.txt
-python3 -m PyInstaller --onefile --hidden-import="googleapiclient" src/main.py
+python3 -m PyInstaller --onefile --hidden-import="googleapiclient" main.py
 tar -czvf dist/archive.tar.gz dist/main
