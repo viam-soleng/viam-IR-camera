49acd5f1c5b37dd4a406d1442a092605d33b090b
Ana Zeneli <ana.zeneli@viam.com>
2025-09-24 16:20:59 -0400

    Bumped to sdk 0.55.1. Fixed setup bugs.

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 UNAME=$(uname -s)
-
+  
 if [ "$UNAME" = "Linux" ]
 then
     echo "Installing venv on Linux"
@@ -15,4 +15,4 @@ fi
 
 python3 -m venv .venv
 . .venv/bin/activate
-pip3 install -r requirements.txt
+pip3 install --trusted-host pypi.org --trusted-host files.pythonhosted.org --trusted-host piwheels.org --trusted-host www.piwheels.org --trusted-host archive1.piwheels.org -r requirements.txt
